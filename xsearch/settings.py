9fd754f906d1698939ec83a2f2f9e85f4509f49b
shooter <shooter@bitcoincentury.org>
2018-03-02 12:02:34 +0800

    import  local_settings

--- xsearch/settings.py
+++ xsearch/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+##############
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
